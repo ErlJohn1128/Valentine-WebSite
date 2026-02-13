b838d8b1e33d99812af4e7dc5f73883e7630f7f5
EarlJ <erljohn004@gmail.com>
2026-02-13 22:57:48 +0800

    added message and fix some things yk

--- valentine.js
+++ valentine.js
@@ -2,10 +2,16 @@ const yes = document.getElementById("yes");
 const no = document.getElementById("no");
 const pageTwo = document.getElementById("pageTwo");
 const pageOne = document.getElementById("pageOne");
+const messageBtn = document.querySelector(".message");
+const hiddenMessage = document.getElementById("hiddenMessage");
 
 yes.onclick = () => {
    pageTwo.style.display = "flex";
    pageOne.style.display = "none";
+};
+
+messageBtn.onclick = () => {
+    hiddenMessage.style.display = "block"; 
 };
 
 no.onmouseover = () => {
